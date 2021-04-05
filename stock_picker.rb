1d6f8f04496319b6b4999f902cae84356503baf3
srirajbur <75371806+srirajbur@users.noreply.github.com>
2021-04-04 23:46:34 -0400

    update stock_picker algorithm

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,16 +1,16 @@
-def stock_picker(arr)
-    result = Hash.new(0)
-    for b in 0..arr.length-1
-        for s in b..arr.length-1 
-            profit = arr[s]-arr[b]
-            key = [b, s]
-            result[key] = profit
-        end
+def stock_picker(stocks)
+  profits = Hash.new
+  stocks.each_with_index do |buy_price, buy_day|
+    stocks.each_with_index do |sell_price, sell_day|
+      if sell_day > buy_day
+        profit = sell_price - buy_price
+        days = [buy_day, sell_day]
+        profits[profit] = days
+      end
     end
-    result.max_by { |k, v| v }[0]
-
+  end
+    
+  profits[profits.keys.max]
 end
 
-prices = [17,3,6,9,15,8,6,1,10]
-
-stock_picker(prices)
+stock_picker([17,3,6,9,15,8,6,1,10])
