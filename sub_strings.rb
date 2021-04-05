64958a5d3bd488bf1c7ca99d38e4c94d93a589f5
srirajbur <75371806+srirajbur@users.noreply.github.com>
2021-04-05 00:04:55 -0400

    update sub_string algorithm

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,12 +1,16 @@
-def substrings(text, array)
-    hash = {}
-    text = text.downcase
-
-    array.select do |word|
-        hash[word] = text.scan(word).length if text.include?(word)
+def substrings(string, dictionary)
+  str_arr = string.split(' ')
+  count = Hash.new(0)
+  dictionary.each do |chars|
+    str_arr.each do |word|
+      if word.downcase.include?(chars.downcase)
+        count[chars] += 1
+      end
     end
+  end
 
-    hash
+  count
 end
-puts substrings("below", dictionary)
-puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
